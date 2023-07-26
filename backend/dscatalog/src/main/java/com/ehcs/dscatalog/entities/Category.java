a7a26fd5fae822b7096c5cd2d1a6e4d8f514da2e
Estevao Souza <estevaosouza18@gmail.com>
2023-07-26 00:59:05 -0300

    Starting category

---
+++ backend/dscatalog/src/main/java/com/ehcs/dscatalog/entities/Category.java
@@ -0,0 +1,15 @@
+package com.ehcs.dscatalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    private Long id;
+    private String name;
+
+}
