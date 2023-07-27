f4794932165b0010df2180d4acbdd601807bc66e
Estevao Souza <estevaosouza18@gmail.com>
2023-07-26 21:14:19 -0300

    Created CategoryService and Repository and add h2 configurations

--- backend/dscatalog/src/main/java/com/ehcs/dscatalog/entities/Category.java
+++ backend/dscatalog/src/main/java/com/ehcs/dscatalog/entities/Category.java
@@ -1,5 +1,6 @@
 package com.ehcs.dscatalog.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,8 +8,12 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
