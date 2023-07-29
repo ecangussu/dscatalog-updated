9a4a0cc0f2ca9d0f0362ab24b6576f27c8bdbcdc
Estevao Souza <estevaosouza18@gmail.com>
2023-07-29 18:30:02 -0300

    Created categoryById endpoint

--- backend/dscatalog/src/main/java/com/ehcs/dscatalog/resources/CategoryResource.java
+++ backend/dscatalog/src/main/java/com/ehcs/dscatalog/resources/CategoryResource.java
@@ -5,13 +5,14 @@ import com.ehcs.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping("/categories")
 public class CategoryResource {
 
     @Autowired
@@ -21,6 +22,12 @@ public class CategoryResource {
     public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> categories = service.findAll();
         return ResponseEntity.ok().body(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO categoryDTO = service.findById(id);
+        return ResponseEntity.ok().body(categoryDTO);
     }
 
 }
--- backend/dscatalog/src/main/java/com/ehcs/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/ehcs/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -20,6 +21,12 @@ public class CategoryService {
         List<Category> categories = repository.findAll();
         List<CategoryDTO> categoriesDTO = categories.stream().map(categorie -> new CategoryDTO(categorie)).toList();
         return categoriesDTO;
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> category = repository.findById(id);
+        return new CategoryDTO(category.get());
     }
 
 }
