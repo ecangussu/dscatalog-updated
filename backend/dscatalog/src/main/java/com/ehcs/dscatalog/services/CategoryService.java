68d3cd22a98060676268f05b9345c5290d81d282
Estevao Souza <estevaosouza18@gmail.com>
2023-10-09 22:05:44 -0300

    POST Category

--- backend/dscatalog/src/main/java/com/ehcs/dscatalog/services/CategoryService.java
+++ backend/dscatalog/src/main/java/com/ehcs/dscatalog/services/CategoryService.java
@@ -30,4 +30,12 @@ public class CategoryService {
         return new CategoryDTO(category.orElseThrow(() -> new ResourceNotFoundException("Resource Not found")));
     }
 
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
 }
